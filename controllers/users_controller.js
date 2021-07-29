3e2dfc222a3e2faa4b9dfc0a3c663f5072be21d4
nitin kumar <nitinkumar010520@gmail.com>
2021-07-29 14:02:46 +0530

    changing the feature to upload profile pic

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,4 +1,6 @@
 const User = require('../models/user');
+const fs=require('fs');
+const path=require('path');
 
 // let's keep it same as before
 module.exports.profile = function(req, res){
@@ -21,6 +23,9 @@ module.exports.update = async function(req, res){
             user.name=req.body.name;
             user.email=req.body.email;
             if(req.file){
+                if(user.avatar){
+                    fs.unlinkSync(path.join(__dirname,'..',user.avatar));
+                }
                 //this is the sharing the path of the uploaded file intotht
                 user.avatar=User.avatarPath+'/'+req.file.filename;
             }
