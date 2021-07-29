44f1722f6eb6090fc24ae6dc5e845f99ab125869
nitin kumar <nitinkumar010520@gmail.com>
2021-07-29 13:49:55 +0530

    added the feature to upload profile pic

--- README.md
+++
@@ -1,7 +0,0 @@
-# spoects
-This is website which is build for sports/e-sports player with following purpose:
-1.To share your views about sports, worldwide things realted to sports
-2.To connects with players with other player.
-3.To know what happening in sports community and other people views/opnion on it.
-4.To stay updated with sports tournaments beign held.
-5.To have chat with player you wanna know.
--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,21 +1,39 @@
 const User = require('../models/user');
 
-
+// let's keep it same as before
 module.exports.profile = function(req, res){
-    User.findById(req.params.id , function(err,user){
+    User.findById(req.params.id, function(err, user){
         return res.render('user_profile', {
             title: 'User Profile',
-            profile_user:user
-        })
-    })
+            profile_user: user
+        });
+    });
+
 }
 
-module.exports.update=function(req,res){
-    if(req.user.id==req.params.id){
-        User.findByIdAndUpdate(req.params.id , req.body,function(err,user){
+
+module.exports.update = async function(req, res){
+    if(req.user.id == req.params.id){
+     try {
+        let user =await User.findByIdAndUpdate(req.params.id);
+        User.uploadedAvatar(req,res,function(err){
+            if(err){console.log('******multer err: ',err)};
+            user.name=req.body.name;
+            user.email=req.body.email;
+            if(req.file){
+                //this is the sharing the path of the uploaded file intotht
+                user.avatar=User.avatarPath+'/'+req.file.filename;
+            }
+            user.save();
             return res.redirect('back');
-        });
+        });   
+     } catch (error) {
+        req.flash('error',err);
+        return res.redirect('back');
+     }   
+     
     }else{
+        req.flash('error', 'Unauthorized!');
         return res.status(401).send('Unauthorized');
     }
 }
@@ -48,19 +66,21 @@ module.exports.signIn = function(req, res){
 // get the sign up data
 module.exports.create = function(req, res){
     if (req.body.password != req.body.confirm_password){
+        req.flash('error', 'Passwords do not match');
         return res.redirect('back');
     }
 
     User.findOne({email: req.body.email}, function(err, user){
-        if(err){console.log('error in finding user in signing up'); return}
+        if(err){req.flash('error', err); return}
 
         if (!user){
             User.create(req.body, function(err, user){
-                if(err){console.log('error in creating user while signing up'); return}
+                if(err){req.flash('error', err); return}
 
                 return res.redirect('/users/sign-in');
             })
         }else{
+            req.flash('success', 'You have signed up, login to continue!');
             return res.redirect('back');
         }
 
@@ -70,11 +90,14 @@ module.exports.create = function(req, res){
 
 // sign in and create a session for the user
 module.exports.createSession = function(req, res){
+    req.flash('success', 'Logged in Successfully');
     return res.redirect('/');
 }
 
 module.exports.destroySession = function(req, res){
     req.logout();
+    req.flash('success', 'You have logged out!');
+
 
     return res.redirect('/');
 }
