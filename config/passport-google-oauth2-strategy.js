cc1b40cd0e30cffe94916c9e969dc79f31c70366
nitin kumar <nitinkumar010520@gmail.com>
2021-07-30 18:34:12 +0530

    adding google autho features

---
+++ config/passport-google-oauth2-strategy.js
@@ -0,0 +1,44 @@
+const passport=require('passport');
+const googleStrategy= require('passport-google-oauth').OAuth2Strategy;
+const crypto=require('crypto');
+const User=require('../models/user');
+
+//tell passport to use new strategy for google login
+passport.use(new googleStrategy({
+    clientID:"407202589243-gvh70vlshu1lhg7hknuevvg8qq8dpi1m.apps.googleusercontent.com",
+    clientSecret:"E5WEBRVMo4nYuxqqJJesFrlD",
+    callbackURL:"http://localhost:8000/users/auth/google/callback"
+},
+    function(accessToken,refreshToken,profile,done){
+        //find a user
+        User.findOne({email:profile.emails[0].value})
+            .exec(function(err,user){
+                if(err){
+                    console.log('error in google strategy-passport',err);
+                    return;
+                }
+                console.log(profile);
+                if(user){
+                    //if found ,set the user as req.user
+                    return done(null,user);
+                }
+                else
+                {   
+                    //if not found ,create the user and set it as req.user(that is sign in that user)
+                    User.create({
+                        name:profile.displayName,
+                        email:profile.emails[0].value,
+                        password:crypto.randomBytes(20).toString('hex')
+                    },function(err,user){
+                        if(err){
+                        console.log('error in google strategy-passport',err);
+                        return; 
+                        }
+                        return done(null,user);
+                    })
+                }
+            });
+    }
+))
+
+module.exports=passport;
